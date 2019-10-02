e1e2ce78eaf34df2620c75868214d1938d96fdae
Luan Morais de Brito <luan.m.brito4@gmai.com>
2019-10-01 23:21:06 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//Carregando modulos
+const express = require('express')
+const handlebars = require("express-handlebars")
+const bodyParser = require('body-parser')
+const app = express()
+const admin = require('./routes/admin')
+const path = require('path')
+const mongoose = require('mongoose')
+const session = require("express-session")
+const flash = require("connect-flash")
+
+//Configurações
+    //Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+
+        app.use(flash())
+    //Middlweware
+        app.use((req,res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
+    //Body Parser
+        app.use(bodyParser.urlencoded({extended:true}))
+        app.use(bodyParser.json())
+    //handlebars
+        app.engine('handlebars',handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    //Mongoose
+        mongoose.Promise = global.Promise
+        mongoose.connect("mongodb://localhost/blogapp")
+        .then(() => {
+            console.log("Conectado ao mongo")
+        })
+        .catch( (err) =>
+        {
+            console.log("Erro ao conectar " + err)
+        })
+    // Public
+        app.use(express.static(path.join(__dirname,"public")))
+
+//Rotas
+    app.get("/", (req,res) => {
+        res.send("Rota Principal")
+    })
+
+    app.get("/posts", (req,res) => {
+        res.send("Post")
+    })
+
+    app.use('/admin', admin)
+//Outros
+const PORT = 8081
+app.listen(PORT, () => {
+    console.log("Servidor Rodando")
+})
---
+++ routes/admin.js
@@ -0,0 +1,71 @@
+const express = require("express")
+const router  = express.Router()
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
+
+router.get('/',(req, res) => {
+    res.render("admin/index")
+})
+
+router.get('/posts', (req,res) => {
+    res.send("<h1>Paginad e post</h1>")
+})
+
+router.get('/categorias', (req, res) => {
+    Categoria.find().sort({date:'desc'})
+    .then((categorias) => {
+        res.render("admin/categorias", {categorias})
+    })
+    .catch((err) => {
+        req.flash("erro_msg", "Houve um erro ao listar as categorias")
+        res.redirect("/admin")
+    })
+    
+})
+
+router.get("/categorias/add", (req,res) => {
+    res.render("admin/addcategorias")
+})
+
+router.post("/categorias/nova", (req,res) => {
+    
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido" })
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null)
+    {
+        erros.push({texto:"Slug Inválido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria é muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros})
+    }
+    else{
+
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save()
+        .then(() => {
+            req.flash("success_msg", "Categoria criada com sucesso")
+            res.redirect("/admin/categorias")
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente")
+            res.redirect("/admin")
+        })
+    }
+})
+
+
+module.exports = router
